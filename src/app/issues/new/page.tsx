47a505a0eb93f64ef47c3077677d3c774d59475c
眺望者007 <909176763@qq.com>
2025-07-12 20:45:13 +0800

    Handle form submission

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -4,28 +4,57 @@ import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react'
 import gfm from '@bytemd/plugin-gfm'
 import { Editor, Viewer } from '@bytemd/react'
+import { Controller, useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
 
 const plugins = [
   gfm(),
   // Add more plugins here
 ]
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
   const [value, setValue] = useState('')
+  const { register,control,handleSubmit} = useForm<IssueForm>();
+  const router = useRouter()
 
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder="Please enter the title of the issue">
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data)=>{
+      await axios.post('/api/issues', data)
+        .then(response => {
+          console.log('Issue created successfully:', response.data);
+          router.push('/issues'); // Redirect to the issues page after successful creation
+        })
+        .catch(error => {
+          console.error('Error creating issue:', error);
+        });
+    })}>
+      <TextField.Root placeholder="Please enter the title of the issue" {...register('title')}>
       </TextField.Root>
-      <Editor
-        value={value}
-        placeholder="Description"
-        plugins={plugins}
-        onChange={(v) => {
-          setValue(v)
-        }}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <Editor
+            {...field}
+            value={value}
+            placeholder="Description"
+            plugins={plugins}
+            onChange={(v) => {
+              setValue(v)
+              field.onChange(v)
+            }}
+          />
+        )}
       />
+
       <Button>Submit new Issue</Button>
-    </div>
+    </form>
   )
 }
 
