191fc88b1ab300053494207e37a87f6878d72400
眺望者007 <909176763@qq.com>
2025-07-14 14:45:07 +0800

    Implement client-side validation

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,29 +1,32 @@
 'use client'
 import 'bytemd/dist/index.css'
-import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react'
 import gfm from '@bytemd/plugin-gfm'
 import { Editor, Viewer } from '@bytemd/react'
 import { Controller, useForm } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createIssueSchema } from '@/app/validationSchemas'
+import { z } from 'zod'
+
+type IssueForm = z.infer<typeof createIssueSchema>
 
 
 const plugins = [
   gfm(),
   // Add more plugins here
 ]
-interface IssueForm {
-  title: string;
-  description: string;
-}
 
 const NewIssuePage = () => {
   const [value, setValue] = useState('')
-  const { register,control,handleSubmit} = useForm<IssueForm>();
+  const { register,control,handleSubmit,formState:{errors}} = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
   const router = useRouter()
   const [error,setError]= useState("")
-
+  
   return (
     <div className='max-w-xl'>
       {
@@ -50,6 +53,7 @@ const NewIssuePage = () => {
       })}>
         <TextField.Root placeholder="Please enter the title of the issue" {...register('title')}>
         </TextField.Root>
+        {errors.title && <Text color="red" as='p'>{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -66,6 +70,7 @@ const NewIssuePage = () => {
             />
           )}
         />
+        {errors.description && <Text color="red" as='p'>{errors.description.message}</Text>}
         <Button>Submit new Issue</Button>
       </form>
     </div>
