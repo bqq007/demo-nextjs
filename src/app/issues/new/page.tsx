e571a79dcbe341bc72a4e50afd7dd06514ed2b32
眺望者007 <909176763@qq.com>
2025-07-14 09:44:43 +0800

    Handle errors

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import 'bytemd/dist/index.css'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react'
 import gfm from '@bytemd/plugin-gfm'
 import { Editor, Viewer } from '@bytemd/react'
@@ -22,39 +22,53 @@ const NewIssuePage = () => {
   const [value, setValue] = useState('')
   const { register,control,handleSubmit} = useForm<IssueForm>();
   const router = useRouter()
+  const [error,setError]= useState("")
 
   return (
-    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data)=>{
-      await axios.post('/api/issues', data)
-        .then(response => {
-          console.log('Issue created successfully:', response.data);
-          router.push('/issues'); // Redirect to the issues page after successful creation
-        })
-        .catch(error => {
+    <div className='max-w-xl'>
+      {
+        error && (
+          <Callout.Root color='red' className='mb-5'>
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )
+      }
+      <form className='space-y-3' onSubmit={handleSubmit(async (data)=>{
+        try {
+          await axios.post('/api/issues', data)
+            .then(response => {
+              console.log('Issue created successfully:', response.data);
+              router.push('/issues'); // Redirect to the issues page after successful creation
+            })
+            // .catch(error => {
+            //   console.error('Error creating issue:', error);
+            // });        
+        } catch (error) {
           console.error('Error creating issue:', error);
-        });
-    })}>
-      <TextField.Root placeholder="Please enter the title of the issue" {...register('title')}>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <Editor
-            {...field}
-            value={value}
-            placeholder="Description"
-            plugins={plugins}
-            onChange={(v) => {
-              setValue(v)
-              field.onChange(v)
-            }}
-          />
-        )}
-      />
-
-      <Button>Submit new Issue</Button>
-    </form>
+          setError('An unexpected error occurred while creating the issue. Please try again later.');
+        }
+      })}>
+        <TextField.Root placeholder="Please enter the title of the issue" {...register('title')}>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <Editor
+              {...field}
+              value={value}
+              placeholder="Description"
+              plugins={plugins}
+              onChange={(v) => {
+                setValue(v)
+                field.onChange(v)
+              }}
+            />
+          )}
+        />
+        <Button>Submit new Issue</Button>
+      </form>
+    </div>
   )
 }
 
